5863c0886ecbdb2d9c7fecebbb04cb00a4d10532
ModSallam <mohamedsallam260@gmail.com>
2022-07-16 23:06:13 +0200

    dio consumer

--- lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
+++ lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
@@ -1,33 +1,19 @@
-import 'dart:convert';
-
+import 'package:random_quote_clean_architecture_bloc_app/core/api/base_api_consumer.dart';
 import 'package:random_quote_clean_architecture_bloc_app/core/api/end_points.dart';
-import 'package:random_quote_clean_architecture_bloc_app/core/error/exceptions.dart';
-import 'package:random_quote_clean_architecture_bloc_app/core/utils/app_strings.dart';
 import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/data/models/quote_model.dart';
-import 'package:http/http.dart' as http;
 
 abstract class BaseRandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
 }
 
 class RandomQuoteRemoteDataSource implements BaseRandomQuoteRemoteDataSource {
-  final http.Client client;
+  final BaseApiConsumer apiConsumer;
 
-  const RandomQuoteRemoteDataSource({required this.client});
+  const RandomQuoteRemoteDataSource({required this.apiConsumer});
 
   @override
   Future<QuoteModel> getRandomQuote() async {
-    final randomQuoteUrl = Uri.parse(EndPoints.randomQuoteUrl);
-    final response = await client.get(
-      randomQuoteUrl,
-      headers: {
-        AppStrings.contentType: AppStrings.applicationJson,
-      },
-    );
-    if (response.statusCode == 200) {
-      return QuoteModel.fromJson(json.decode(response.body));
-    } else {
-      throw ServerException();
-    }
+    final response = await apiConsumer.get(EndPoints.randomQuoteUrl);
+    return QuoteModel.fromJson(response);
   }
 }
--- lib/injector.dart
+++ lib/injector.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:random_quote_clean_architecture_bloc_app/core/api/app_interceptors.dart';
+import 'package:random_quote_clean_architecture_bloc_app/core/api/base_api_consumer.dart';
+import 'package:random_quote_clean_architecture_bloc_app/core/api/dio_consumer.dart';
 import 'package:random_quote_clean_architecture_bloc_app/core/network/network_info.dart';
 import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/data/datasources/random_quote_local_data_source.dart';
 import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/data/datasources/random_quote_remote_data_source.dart';
@@ -8,7 +12,7 @@ import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/d
 import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/domain/usecases/get_random_quote_use_case.dart';
 import 'package:random_quote_clean_architecture_bloc_app/features/random_quote/presentation/cubit/random_quote_cubit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 final serviceLocator = GetIt.instance;
 
@@ -35,12 +39,16 @@ Future<void> setup() async {
   serviceLocator.registerLazySingleton<BaseRandomQuoteLocalDataSource>(
       () => RandomQuoteLocalDataSource(sharedPreferences: serviceLocator()));
   serviceLocator.registerLazySingleton<BaseRandomQuoteRemoteDataSource>(
-      () => RandomQuoteRemoteDataSource(client: serviceLocator()));
+      () => RandomQuoteRemoteDataSource(apiConsumer: serviceLocator()));
 
   //! Core
   //Network
   serviceLocator.registerLazySingleton<BaseNetworkInfo>(
       () => NetworkInfo(connectionChecker: serviceLocator()));
+
+  // Api Consumer
+  serviceLocator.registerLazySingleton<BaseApiConsumer>(
+      () => DioConsumer(client: serviceLocator()));
 
   //! External
   // Shared Preferences
@@ -48,7 +56,21 @@ Future<void> setup() async {
   serviceLocator.registerLazySingleton(() => sharedPreferences);
 
   // http
-  serviceLocator.registerLazySingleton(() => http.Client());
+  // serviceLocator.registerLazySingleton(() => http.Client());
+
+  // Dio
+  serviceLocator.registerLazySingleton(() => Dio());
+  serviceLocator.registerLazySingleton(() => AppInterceptors());
+  serviceLocator.registerLazySingleton(
+    () => LogInterceptor(
+      request: true,
+      requestBody: true,
+      requestHeader: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+    ),
+  );
 
   // Internet Connection Checker
   serviceLocator.registerLazySingleton(() => InternetConnectionChecker());
